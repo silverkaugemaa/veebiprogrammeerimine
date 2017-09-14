684a07a95e61b5abb251d800b4d2efd1818e9dc5
Silver Kaugemaa <silverkaugemaa@users.noreply.github.com>
2017-09-14 10:47:05 +0300

    lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,21 @@
+<!DOCKTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Silver Kaugemaa programmerib veebilehte
+	</title>
+</head>
+<body>
+	<hl>Silver Kaugemaa, veebiprogrammerimine
+	</hl>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisust tõsiseltvõetavat sisu! </p>
+	<p>Enda heaolu nime ärge kopeerige siit MITTE MIDAGI! </p>
+	<?php
+		echo "algas php õppimine.";
+
+	?>
+
+
+
+</body>
+</html>
